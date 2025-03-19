5c447b8f171d5a298a42160dcff60e12f252c2bf
lag129 <65485662+lag129@users.noreply.github.com>
2025-03-20 01:00:41 +0900

    fontSize周りの記述をコメントアウト

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,11 +15,11 @@ fun main() {
     val filePath = "src/main/resources/sample.docx"
     val analyzeDocument = AnalyzeDocument(filePath)
     val documentContent = analyzeDocument.parseWordDocument()
-    val fontSizes = analyzeDocument.getFontSizes()
+//    val fontSizes = analyzeDocument.getFontSizes()
     val percent = analyzeDocument.getPercent()
     val json = Json.encodeToString(documentContent)
     println(json)
-    println("Font sizes: $fontSizes")
+//    println("Font sizes: $fontSizes")
     println("Zoom percent: $percent")
     File("src/main/resources/sample.json").writeText(json)
 }
